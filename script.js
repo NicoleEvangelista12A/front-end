c69a5e2f99bf73838bd2fd94d121d2ccef3f3daf
NicoleEvangelista12A <nicole.barross@eaportal.org>
2025-10-23 01:05:05 -0300

    salvandoooou

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.querySelecton("button")
-button.addEventListener("click", receiveData)
+button.addEventListener( "click", receiveData )
 
 function receiveData() {
   fetch("http://localhost:3000").then(response => response.json()).then(data => console.log(data.message))
