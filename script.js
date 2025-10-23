340d9a1676ac4fa99f5451ce3393e63f7fd4feeb
NicoleEvangelista12A <nicole.barross@eaportal.org>
2025-10-23 01:02:19 -0300

    salvandooo

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.querySelecton("button")
+button.addEventListener("click", receiveData)
+
+function receiveData() {
+  fetch("http://localhost:3000").then(response => response.json()).then(data => console.log(data.message))
+
+}
